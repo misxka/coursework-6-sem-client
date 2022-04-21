441181e32b555e1bb029f364a5d64e62bad223dc
misxka <mishutkavlesu@gmail.com>
2022-04-21 22:09:52 +0300

    feat: add auth ui handlers

--- next-client/components/Navbar/Navbar.tsx
+++ next-client/components/Navbar/Navbar.tsx
@@ -1,13 +1,40 @@
-import { Button, Icon } from '@chakra-ui/react';
+import { Avatar, Button, Icon, Skeleton } from '@chakra-ui/react';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
 import Link from 'next/link';
-import { FaUserPlus } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { FaSignOutAlt, FaUserPlus } from 'react-icons/fa';
 
 import styles from './Navbar.module.scss';
 
 function Navbar() {
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.buttons}>
+  const [authGroup, setAuthGroup] = useState<JSX.Element>(<Skeleton height='40px' />);
+
+  useEffect(() => {
+    let isExpired;
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      const dateNow = new Date();
+      if (decodedToken.exp && decodedToken.exp * 1000 < dateNow.getTime()) isExpired = true;
+      else isExpired = false;
+    }
+
+    if (!isExpired) {
+      setAuthGroup(
+        <>
+          <Avatar name='Mikhail Viaryha' className={styles.avatar} />
+          <Link href='/login'>
+            <a>
+              <Button rightIcon={<Icon as={FaSignOutAlt} />} colorScheme='red' variant='outline'>
+                Выйти
+              </Button>
+            </a>
+          </Link>
+        </>
+      );
+    } else {
+      setAuthGroup(
         <Link href='/login'>
           <a>
             <Button rightIcon={<Icon as={FaUserPlus} />} colorScheme='blue' variant='solid'>
@@ -15,7 +42,13 @@ function Navbar() {
             </Button>
           </a>
         </Link>
-      </div>
+      );
+    }
+  }, []);
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.buttons}>{authGroup}</div>
     </div>
   );
 }
