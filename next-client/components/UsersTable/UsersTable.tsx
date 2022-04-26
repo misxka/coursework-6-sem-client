dfa5fe967c8c13050385250370386535f1dc881f
misxka <mishutkavlesu@gmail.com>
2022-04-26 22:14:41 +0300

    feat: implement table's pagination and size to display, add skeleton when fetching data

--- next-client/components/UsersTable/UsersTable.tsx
+++ next-client/components/UsersTable/UsersTable.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup, Editable, EditableInput, EditablePreview, Icon, IconButton, Select, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { Button, ButtonGroup, Editable, EditableInput, EditablePreview, Icon, IconButton, Select, Skeleton, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { FaAngleLeft, FaAngleRight, FaEllipsisH } from 'react-icons/fa';
 
@@ -12,17 +12,22 @@ export default function UsersTable() {
   const [size, setSize] = useState<number>(10);
   const [users, setUsers] = useState<IUser[]>([]);
   const [lastPage, setLastPage] = useState<number>(1);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
+      setIsFetching(true);
+
       const { content, totalPages, first, last } = await getUsersByPageAndSize(page - 1, size);
 
       setUsers(content);
       setLastPage(totalPages);
+
+      setIsFetching(false);
     };
 
     fetchUsers();
-  }, [page]);
+  }, [page, size]);
 
   const handlePreviousClick = () => {
     if (page > 1) setPage(page - 1);
@@ -32,9 +37,36 @@ export default function UsersTable() {
     if (page < lastPage) setPage(page + 1);
   };
 
+  const composeTableSkeleton = () => {
+    const filler = Array.apply(null, Array(size));
+    return (
+      <>
+        {filler.map((elem, index) => (
+          <Tr key={index}>
+            <Td>
+              <Skeleton w={10} height='22px' />
+            </Td>
+            <Td>
+              <Skeleton w={200} height='22px' />
+            </Td>
+            <Td>
+              <Skeleton w={300} height='22px' />
+            </Td>
+            <Td>
+              <Skeleton w={260} height='22px' />
+            </Td>
+            <Td>
+              <Skeleton w={160} height='22px' />
+            </Td>
+          </Tr>
+        ))}
+      </>
+    );
+  };
+
   return (
     <div className={styles.content}>
-      <Select marginLeft='auto' w={100} defaultValue={10} size='sm'>
+      <Select onChange={e => setSize(+e.target.value)} marginLeft='auto' w={100} defaultValue={10} size='sm'>
         <option value='10'>10</option>
         <option value='20'>20</option>
         <option value='50'>50</option>
@@ -51,40 +83,42 @@ export default function UsersTable() {
             </Tr>
           </Thead>
           <Tbody>
-            {users.map(user => (
-              <Tr>
-                <Td>
-                  <Editable textAlign='center' w='40px' defaultValue={user.id?.toString()}>
-                    <EditablePreview />
-                    <EditableInput />
-                  </Editable>
-                </Td>
-                <Td>
-                  <Editable textAlign='center' w='200px' defaultValue={user.login}>
-                    <EditablePreview />
-                    <EditableInput />
-                  </Editable>
-                </Td>
-                <Td>
-                  <Editable textAlign='center' w='300px' defaultValue={user.email}>
-                    <EditablePreview />
-                    <EditableInput />
-                  </Editable>
-                </Td>
-                <Td>
-                  <Editable textAlign='center' w='260px' defaultValue={user.fullname}>
-                    <EditablePreview />
-                    <EditableInput />
-                  </Editable>
-                </Td>
-                <Td>
-                  <Editable textAlign='center' w='160px' defaultValue={user.role}>
-                    <EditablePreview />
-                    <EditableInput />
-                  </Editable>
-                </Td>
-              </Tr>
-            ))}
+            {isFetching
+              ? composeTableSkeleton()
+              : users.map(user => (
+                  <Tr key={user.id}>
+                    <Td>
+                      <Editable textAlign='center' w='40px' defaultValue={user.id?.toString()}>
+                        <EditablePreview />
+                        <EditableInput />
+                      </Editable>
+                    </Td>
+                    <Td>
+                      <Editable textAlign='center' w='200px' defaultValue={user.login}>
+                        <EditablePreview />
+                        <EditableInput />
+                      </Editable>
+                    </Td>
+                    <Td>
+                      <Editable textAlign='center' w='300px' defaultValue={user.email}>
+                        <EditablePreview />
+                        <EditableInput />
+                      </Editable>
+                    </Td>
+                    <Td>
+                      <Editable textAlign='center' w='260px' defaultValue={user.fullname}>
+                        <EditablePreview />
+                        <EditableInput />
+                      </Editable>
+                    </Td>
+                    <Td>
+                      <Editable textAlign='center' w='160px' defaultValue={user.role}>
+                        <EditablePreview />
+                        <EditableInput />
+                      </Editable>
+                    </Td>
+                  </Tr>
+                ))}
           </Tbody>
         </Table>
       </TableContainer>
